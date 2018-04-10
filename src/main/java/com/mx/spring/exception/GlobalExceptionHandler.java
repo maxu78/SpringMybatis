93327011ca58ad540a7fbc7c6104117fa98916f9
maxu78 <805955913@qq.com>
2018-04-10 15:17:00 +0800

    controller异常捕获

---
+++ src/main/java/com/mx/spring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.mx.spring.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler
+    public Object processException(Exception e){
+        System.out.println("捕获到了异常!!!!");
+        Map map = new HashMap();
+        map.put("status", "500");
+        map.put("data", stackTraceToString(e));
+        return map;
+    }
+
+    public static String stackTraceToString(Throwable e) {
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            return sw.toString();
+        } catch (Exception var3) {
+            return "bad stack2string";
+        }
+    }
+}
